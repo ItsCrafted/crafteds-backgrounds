2a30229f89014252b37afda709914de13ccaafec
Crafted <banditkingharper@gmail.com>
2026-01-11 14:35:22 -0700

    A

--- workers-site/index.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request) {
-    return new Response("This is a dummy Worker to satisfy Wrangler CLI cuz its being bitchy.", {
-      headers: { "content-type": "text/plain" },
-    });
-  },
-};
